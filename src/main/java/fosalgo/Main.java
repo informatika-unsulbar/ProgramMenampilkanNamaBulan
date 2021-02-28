f62ca7588eea3a27129af96070be6700027e88f0
Sugiarto Cokrowibowo <sugiarto.cokrowibowo.id@gmail.com>
2021-03-01 06:49:03 +0800

    new commit

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fosalgo;
+
+import java.util.Scanner;
 
 /**
  *
- * @author VD1
+ * @author Sugiarto Cokrowibowo
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        //TULISKAN KODEMU DIBAWAH INI
+        
+        
+        
     }
-    
+
 }
